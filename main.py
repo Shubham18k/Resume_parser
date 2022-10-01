e4ea575d7b49193a45eb90a2dd6a584ad9131561
Shubham R Kadam <36569119+Shubham18k@users.noreply.github.com>
2022-10-01 11:46:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from pdfminer.high_level import extract_text
 import docx2txt
 import os
+import json
 
 
 app=FastAPI()
@@ -18,48 +19,51 @@ def hello(request:Request):
     return template.TemplateResponse("home.html",{"request":request})
 
 
-def file_pdf(file):
+def file_pdf(files):
     L=[]
-    name,ext=file.filename.split('.')
-    c=os.getcwd()
-    if ext=='pdf':
-        try:
-            os.chdir(os.path.join(c,'Data'))
-            f_dest = open(file.filename, 'wb')
-            shutil.copyfileobj(file.file,f_dest)
-            outfile_name = f"{file.filename}"
-            txt=extract_text(outfile_name)
-            p=document.main(txt)
-            L.append(p)
-        except Exception as e:
-            print(e)
-
-    elif ext=='docx':
-        try:
-            os.chdir(os.path.join(c,'Data'))
-            f_dest=open(file.filename,'wb')
-            shutil.copyfileobj(file.file,f_dest)
-            doc_name=f'{file.filename}'
-            txt=docx2txt.process(doc_name)
-            d=document.main(txt)
-            L.append(d)
-        except Exception as e:
+    for file in files:
+        name,ext=file.filename.split('.')
+        c=os.getcwd()
+        if ext=='pdf':
+            try:
+                os.chdir(os.path.join(c,'Data'))
+                f_dest = open(file.filename, 'wb')
+                shutil.copyfileobj(file.file,f_dest)
+                outfile_name = f"{file.filename}"
+                txt=extract_text(outfile_name)
+                p=document.main(txt)
+                L.append(p)
+                os.chdir('../')
+            except Exception as e:
                 print(e)
 
-    else:
-        return 'Invalid format'
-    os.chdir('../')
+        elif ext=='docx':
+            try:
+                os.chdir(os.path.join(c,'Data'))
+                f_dest=open(file.filename,'wb')
+                shutil.copyfileobj(file.file,f_dest)
+                doc_name=f'{file.filename}'
+                txt=docx2txt.process(doc_name)
+                d=document.main(txt)
+                L.append(d)
+                os.chdir('../')
+            except Exception as e:
+                    print(e)
+
+        else:
+            print('Invalid format')
+    
     return set(L)
 
 
 
 
 @app.post('/data/')
-async def upload(file:UploadFile=File(...)):
+async def upload(file:List[UploadFile]=File(...)):
     try:
         data=file_pdf(file)
-        path=os.getcwd()
-        os.chdir(path)
+        #path=os.getcwd()
+        #os.chdir(path)
         return data
     except Exception as e:
         print(e)
